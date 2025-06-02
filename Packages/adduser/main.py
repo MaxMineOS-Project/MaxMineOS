ab4b0002a881a554f6c4b062420cf10a7b4fd229
Parhomenko Maxim <maxim.parkhomenko@internet.ru>
2025-06-02 10:29:42 +0300

    Fixing issue in adduser
    Fixing adduser issue!

--- Packages/adduser/main.py
+++ Packages/adduser/main.py
@@ -5,9 +5,54 @@ import os
 
 
 def main(argv:list[str], abspath:str):
+    rejected_symbols = (
+        "~",
+        "`",
+        "!",
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "0",
+        "@",
+        "\"",
+        "\\",
+        "-",
+        "_",
+        "=",
+        "+",
+        ";",
+        "%",
+        "^",
+        ":",
+        "&",
+        "?",
+        ",",
+        ".",
+        "/",
+        ">",
+        "<",
+        "\t",
+        "'",
+        "[",
+        "]",
+        "{",
+        "}",
+        "(",
+        ")",
+        "*",
+        "$"
+    )
     if len(argv) == 1:
         print("Передайте имя пользователя в аргументы!")
         return 1
+    if any(char in rejected_symbols for char in argv[1]):
+        print("Не используйте запрещенные символы в имени!")
     if os.path.exists(os.path.join(abspath, "Users", argv[1])):
         print(f"Пользователь с именем {argv[1]} уже существует!")
         return 1
